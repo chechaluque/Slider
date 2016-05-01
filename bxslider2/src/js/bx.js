a55e8fcab73ec7fa785ead9a31a2820f0183dadd
chechaluque <cesarluque2000@gmail.com>
2016-04-30 19:23:28 -0700

    Slider
    Slider created with pluging

---
+++ bxslider2/src/js/bx.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#bxslider").bxSlider({
+  	auto: true,
+  	autoControls: true,
+  	captions:true,
+  	minSlides: 1,
+  	maxSlides: 1,
+  	slideWidth: 700,
+  	slideMarging: 10
+  });
+});
